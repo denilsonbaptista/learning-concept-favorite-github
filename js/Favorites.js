7e97a7c3701db0a1fdb637d1dd7735f7953108e4
Denilson Baptista <denilsonbaptistaalfa@outlook.com>
2023-09-12 22:24:59 -0300

    Criando HTML com JavaScript

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,6 +1,24 @@
 export class Favorites {
   constructor(root) {
     this.root = document.querySelector(root)
+    this.load()
+  }
+
+  load() {
+    this.entries = [
+      {
+        login: "denilsonbaptista",
+        name: "Denilson Baptista",
+        public_repos: "76",
+        followers: "9589",
+      },
+      {
+        login: "maykbrito",
+        name: "Mayk Brito",
+        public_repos: "76",
+        followers: "9589",
+      },
+    ]
   }
 }
 
@@ -8,17 +26,53 @@ export class FavoritesView extends Favorites {
   constructor(root) {
     super(root)
 
+    this.tbody = this.root.querySelector("table tbody")
+
     this.update()
   }
 
   update() {
     this.removeAllTr()
+
+    this.entries.forEach((user) => {
+      const row = this.createRow()
+
+      row.querySelector(
+        ".user img"
+      ).src = `https://github.com/${user.login}.png`
+      row.querySelector(".user img").atl = `Imagem de ${user.name}`
+      row.querySelector(".user p").textContent = user.name
+      row.querySelector(".user span").textContent = user.login
+      row.querySelector(".repositories").textContent = user.public_repos
+      row.querySelector(".followers").textContent = user.followers
+
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
+    const tr = document.createElement("tr")
+
+    tr.innerHTML = `
+      <td class="user">
+        <img src="https://github.com/denilsonbaptista.png" alt="" />
+        <a href="https://github.com/denilsonbaptista" target="_blank"
+          ><p>Denilson Baptista</p>
+          <span>denilsonbaptista</span></a
+        >
+      </td>
+      <td class="repositories">39</td>
+      <td class="followers">18</td>
+      <td>
+        <button class="remove">&times;</button>
+      </td>
+    `
+
+    return tr
   }
 
   removeAllTr() {
-    const tbody = this.root.querySelector("table tbody")
-
-    tbody.querySelectorAll("tr").forEach((tr) => {
+    this.tbody.querySelectorAll("tr").forEach((tr) => {
       tr.remove()
     })
   }
