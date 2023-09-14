8b3eb8cd81e61ebfa5e2d1512309104279ec16ca
Denilson Baptista <denilsonbaptistaalfa@outlook.com>
2023-09-13 21:28:49 -0300

    Imutabilidade para trabalhar com os dados da aplicação

--- js/Favorites.js
+++ js/Favorites.js
@@ -20,6 +20,14 @@ export class Favorites {
       },
     ]
   }
+
+  delete(user) {
+    const filteredEntries = this.entries.filter(
+      (entry) => entry.login !== user.login
+    )
+
+    console.log(filteredEntries)
+  }
 }
 
 export class FavoritesView extends Favorites {
@@ -45,6 +53,14 @@ export class FavoritesView extends Favorites {
       row.querySelector(".user span").textContent = user.login
       row.querySelector(".repositories").textContent = user.public_repos
       row.querySelector(".followers").textContent = user.followers
+
+      row.querySelector(".remove").onclick = () => {
+        const isOk = confirm("Tem certeza que deseja deletar está linha?")
+
+        if (isOk) {
+          this.delete(user)
+        }
+      }
 
       this.tbody.append(row)
     })
