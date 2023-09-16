d5fa99fc62a5bfcf5e0cf540a3df6ef7d2040488
Denilson Baptista <denilsonbaptistaalfa@outlook.com>
2023-09-15 21:07:28 -0300

    Cuidando do fluxo da aplicação com try, catch, throw

--- js/Favorites.js
+++ js/Favorites.js
@@ -23,8 +23,24 @@ export class Favorites {
     this.entries = JSON.parse(localStorage.getItem("@github-favorites:")) || []
   }
 
+  save() {
+    localStorage.setItem("@github-favorites:", JSON.stringify(this.entries))
+  }
+
   async add(username) {
-    const user = await GithubUser.search(username)
+    try {
+      const user = await GithubUser.search(username)
+
+      if (user.login === undefined) {
+        throw new Error("Usuário não encontrado")
+      }
+
+      this.entries = [user, ...this.entries]
+      this.update()
+      this.save()
+    } catch (error) {
+      alert(error.message)
+    }
   }
 
   delete(user) {
@@ -34,6 +50,7 @@ export class Favorites {
 
     this.entries = filteredEntries
     this.update()
+    this.save()
   }
 }
 
